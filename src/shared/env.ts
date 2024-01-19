b43a304bdae04e46da0212a620d4843c4ab7b560
ibnu.musyaffa <ibnu.musyaffa@gmail.com>
2024-01-19 16:45:35 +0700

    update

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -9,35 +9,35 @@ export const env = parseEnv(process.env, {
   PORT: port().default(3000),
 
   APP_NAME: z.string().default("MyApp"),
-  APP_KEY: z.string(),
+  APP_KEY: z.string().min(1),
   APP_DEBUG: z.boolean().default(true),
   APP_URL: z.string().default("http://localhost:3000"),
 
   //DB
-  DB_USER: z.string(),
-  DB_HOST: z.string(),
+  DB_USER: z.string().min(1),
+  DB_HOST: z.string().min(1),
   DB_PORT: port(),
-  DB_PASSWORD: z.string(),
-  DB_NAME: z.string(),
+  DB_PASSWORD: z.string().min(1),
+  DB_NAME: z.string().min(1),
 
   //Redis
-  REDIS_HOST: z.string(),
+  REDIS_HOST: z.string().min(1),
   REDIS_PASSWORD: z.string(),
   REDIS_PORT: port(),
 
   //rabbitMQ
-  RABBITMQ_HOST: z.string(),
+  RABBITMQ_HOST: z.string().min(1),
   RABBITMQ_PORT: port(),
-  RABBITMQ_USERNAME: z.string(),
-  RABBITMQ_PASSWORD: z.string(),
+  RABBITMQ_USERNAME: z.string().min(1),
+  RABBITMQ_PASSWORD: z.string().min(1),
 
   //Mail
-  MAIL_HOST: z.string(),
+  MAIL_HOST: z.string().min(1),
   MAIL_PORT: z.number(),
-  MAIL_USERNAME: z.string(),
-  MAIL_PASSWORD: z.string(),
-  MAIL_FROM_NAME: z.string(),
-  MAIL_FROM_ADDRESS: z.string(),
+  MAIL_USERNAME: z.string().min(1),
+  MAIL_PASSWORD: z.string().min(1),
+  MAIL_FROM_NAME: z.string().min(1),
+  MAIL_FROM_ADDRESS: z.string().min(1),
 })
 
 export default env
